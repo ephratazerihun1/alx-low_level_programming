5dad31221f425cfcb1c3ebc0f4a5473d34957e4a
Ephrata Zerihun <ephzerihun@gmial.com>
2022-06-03 14:04:54 -0700

    positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assignes arandom number to int n everytime
+ * it excutes and prints it
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+
+  if (n == 0)
+    printf("%d is zero\n", n);
+  else if (n < 0)
+    printf("%d is negative\n", n);
+  else
+    printf("%d is positive\n", n);
+
+  return (0);
+}
