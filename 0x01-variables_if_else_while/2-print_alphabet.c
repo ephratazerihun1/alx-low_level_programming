a42e337a27e1ef2d5d99bda67f83492906183ee9
Ephrata Zerihun <ephzerihun@gmial.com>
2022-06-03 15:24:32 -0700

    print  alpha.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,12 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-  char letter;
-
-  for (letter = 'a'; letter <= 'z'; letter++)
-    putchar(letter);
-
-  putchar('\n');
-
-  return (0);
+char letter;
+for(letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
