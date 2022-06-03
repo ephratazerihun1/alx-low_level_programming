4b77321952de3807cc7b332ac83b8cabccfb2b0c
Ephrata Zerihun <ephzerihun@gmial.com>
2022-06-03 16:08:48 -0700

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the last digit of the random
+ *
+ * it excutes and prints it
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alp;
+for (alp = 122; alp >= 97; alp--)
+{
+putchar(alp);
+}
+putchar('\n');
+return (0);
+}
