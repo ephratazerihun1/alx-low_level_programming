9ce4af863ae3853ac71778f0d3457849220da93a
Ephrata Zerihun <ephzerihun@gmial.com>
2022-06-03 16:16:45 -0700

    print base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - prints the last digit of the random
+ * main - Prints all the numbers of base 16 in lowercase.
  *
- * it excutes and prints it
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 
 int main(void)
 {
-char base_16;
-int n;
-for (n = 0; n <= 9; n++)
-{
-putchar(n);
-}
-for (base_16 = 'a'; base_16 <= 'f'; base_16++)
-{
-putchar(base_16);
-}
+int num;
+char letter;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
 putchar('\n');
 return (0);
 }
