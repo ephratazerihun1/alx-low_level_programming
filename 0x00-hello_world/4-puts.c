efe2cded0c859064dbef7f0b3cd6f016f0d8f513
Ephrata Zerihun <ephzerihun@gmial.com>
2022-06-05 13:59:11 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ *prints Programming is like building a multilingual puzzle
+ *uses the puts function
+ *returns 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
