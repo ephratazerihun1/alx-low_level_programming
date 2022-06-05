426b9cd4ad36c225bbc2a3609e18ee742e2c6f3a
Ephrata Zerihun <ephzerihun@gmial.com>
2022-06-05 14:46:28 -0700

    write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - prints the following
+*
+*Return: 1 for error
+*/
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
