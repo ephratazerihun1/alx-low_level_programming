bf7941335fbad89b9e4125060d48e9919b2e355b
Ephrata Zerihun <ephzerihun@gmial.com>
2022-06-07 19:25:47 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet - print alphabet
+*
+* Return: sucess 0.
+*/
+
+void print_alphabet(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
+}
