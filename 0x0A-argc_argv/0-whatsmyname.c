8177ddd1f96a4a218f2c66463d9472bbb226d380
Ephrata Zerihun <ephzerihun@gmial.com>
2022-06-24 14:24:45 -0700

    shell basics

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main - print the name of the program
+* main - Print the name of the program
 * @argc: Count arguments
 * @argv: Arguments
 *
@@ -9,7 +9,7 @@
 */
 int main(int argc, char *argv[])
 {
-(void) argv;
-printf("%i\n", argc - 1);
+(void) argc;
+printf("%s\n", argv[0]);
 return (0);
 }
